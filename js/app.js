0973510440f53eec1ef0494841a9b1eff3d8c9cc
edwardyu714 <eboxyz@gmail.com>
2016-04-22 17:06:36 -0700

    5pm done

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  $('#tile2').click(function(){
+    var content2 = $('#content2')
+
+    content2.animate({fontSize: '1.5em'})
+    content2.addClass('box_rotate box_transition');
+
+  })
+
+
+
+
+})
+
+
+
+
